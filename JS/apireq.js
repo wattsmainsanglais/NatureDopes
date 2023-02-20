9c7b1503c54dabf8df705f67354d575b5ca60fe3
wattsmainsanglais <wattsmainsanglais@gmail.com>
2023-02-20 12:09:04 +0100

    Started JS code for api/backend

---
+++ JS/apireq.js
@@ -0,0 +1,24 @@
+
+const urlToPost = 'http://localhost:4001/markerList'
+
+const postMarker = async () => {
+    const speciesName = document.getElementById('species-name').value;
+    const firstRef = document.getElementById('firstRef').value;
+    const secondRef = document.getElementById('secondRef').value;
+
+    const data = JSON.stringify({speciesName: speciesName, firstRef: firstRef, secondRef: secondRef});
+      
+    try {
+        const response = await fetch(urlToPost, {
+            method: 'POST',
+            body: data,
+            
+        });
+            if(response.ok){
+          const jsonResponse = await response.json();
+          console>log(jsonResponse);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
---
+++ JS/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+/*const speciesName = document.getElementById('species-name');
+const firstRef = document.getElementById('firstRef');
+const secondRef = document.getElementById('secondRef');
+*/
+let markerListId = 2;
+
+function addMarkerToArray(req, res, next){
+    const id = markerListId++;
+    const speciesName = req.body.speciesName;
+    const firstRef = req.body.firstRef;
+    const secondRef = req.body.secondRef;
+    
+    markerList.push({id: id, speciesName: speciesName, firstRef: firstRef, secondRef: secondRef});
+
+}
+
+let markerList = [
+    {
+        id: 1,
+        speciesName: 'Violet',
+        firstRef: 2,
+        secondRef: 2
+    },
+    {
+        id: 2,
+        speciesName: 'Daisy',
+        firstRef: 1,
+        secondRef: 1 
+    }
+
+]
+
+app.listen(PORT, () =>{
+    console.log('Server is listening on port 4001...' )
+})
