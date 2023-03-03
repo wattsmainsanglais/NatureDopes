4779b88f10c21b67422597623802006d6b929523
wattsmainsanglais <wattsmainsanglais@gmail.com>
2023-03-03 20:36:29 +0100

    Development of map endpoints

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 
 const PORT = process.env.PORT || 4001;
+
+app.use(express.static('naturedopes'));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
+  app.use(bodyParser.json());
 
 /*const speciesName = document.getElementById('species-name');
 const firstRef = document.getElementById('firstRef');
 const secondRef = document.getElementById('secondRef');
 */
-let markerListId = 2;
+let markerListId = 3;
 
 function addMarkerToArray(req, res, next){
+    console.log(req.body);
     const id = markerListId++;
-    const speciesName = req.body.speciesName;
-    const firstRef = req.body.firstRef;
-    const secondRef = req.body.secondRef;
+    let species = req.body.speciesName;
+    let first = req.body.firstRef;
+    let second = req.body.secondRef;
     
-    markerList.push({id: id, speciesName: speciesName, firstRef: firstRef, secondRef: secondRef});
-
+    markerList.push({'id': id, 'speciesName': species, 'firstRef': first, 'secondRef': second});
+    next()
 }
 
 let markerList = [
@@ -37,7 +57,8 @@ let markerList = [
 
 app.post('/markerlist', addMarkerToArray, (req, res, next) => {
     console.log(req.body);
-    res.status(200).send('New discovery added to array');
+    res.status(201).send(req.body);
+    console.log(markerList);
 })
 
 app.listen(PORT, () =>{
---
+++ apireq.js
@@ -0,0 +1,53 @@
+
+const urlToPost = 'http://localhost:4001/markerList'
+const fetchMarkerSubmit = document.getElementById('map-button');
+
+ 
+
+
+
+
+const postMarker = async (speciesName, firstRef, secondRef) => {
+
+  const data = JSON.stringify({'speciesName': speciesName, 'firstRef': firstRef, 'secondRef': secondRef});
+      
+    try {
+        const response = await fetch(urlToPost, {
+            method: 'POST',
+            body: data,
+            headers: {
+              'Content-type': 'application/json',     
+            }  
+        });
+            if(response.ok){
+          const jsonResponse = await response.json();
+          console.log(jsonResponse);
+        }
+      } catch (error) {
+        console.log('What is going on?', error);
+      }
+    }
+
+    fetchMarkerSubmit.addEventListener('click', () => {
+      let speciesName = document.getElementById('species-name').value;
+      let firstRef = document.getElementById('firstRef').value;
+      let secondRef = document.getElementById('secondRef').value;
+      console.log(speciesName);
+      postMarker(speciesName, firstRef, secondRef);
+    })
+   
+
+    /*fetchRandomButton.addEventListener('click', () => {
+      fetch('/api/quotes/random')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          renderError(response);
+        }
+      })
+      .then(response => {
+        renderQuotes([response.quote]);
+      });
+    });
+    */
