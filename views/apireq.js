cd5ee2e0159aa5c70d413183c2f6527366d61e8f
mainsanglais <wattsmainsanglais@gmail.com>
2023-07-17 22:19:05 +0200

    Update apireq.js

--- views/apireq.js
+++ views/apireq.js
@@ -80,7 +80,7 @@ function clearData(){
 
   const processPopulateMarkers = (name, first, second, path) => {
   
-      const realPath = process.env.RAILWAY_VOLUME_MOUNT_PATH + '/'+ path;
+      const realPath = '/views/uploads/' + path;
       console.log(realPath);
       let pathTag
       if(path === 'null'){
