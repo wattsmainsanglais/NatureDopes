271e029c7e66c5621aaf014e362c74ea29db3b46
mainsanglais <wattsmainsanglais@gmail.com>
2023-07-17 20:40:11 +0200

    Update apireq.js

--- views/apireq.js
+++ views/apireq.js
@@ -80,7 +80,8 @@ function clearData(){
 
   const processPopulateMarkers = (name, first, second, path) => {
   
-      const realPath = '../data/uploads/'+ path;
+      const realPath = './data/uploads'+ path;
+      console.log(realPath)
       let pathTag
       if(path === 'null'){
         pathTag = '<img width="100" height="100" src="" alt=" No photo for this find, but just as valuable, Thanks!"><h2>' + name +'</h2>';
