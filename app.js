9ba40481fbaf35a73ae371f749b6ac4d985e2477
mainsanglais <wattsmainsanglais@gmail.com>
2023-07-18 15:41:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/data/')
+    cb(null, 'data/')
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
