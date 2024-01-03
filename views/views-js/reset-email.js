edd0e50377f42d9fd438d413c229853b70a012e2
mainsanglais <wattsmainsanglais@gmail.com>
2024-01-03 19:34:53 +0000

    Update reset-email.js
    Add www to url to post

--- views/views-js/reset-email.js
+++ views/views-js/reset-email.js
@@ -1,6 +1,6 @@
 //post email address to server and handle response
 
-const emailPasswordResetURL = 'https://naturedopes.com/reset-password-email'
+const emailPasswordResetURL = 'https://www.naturedopes.com/reset-password-email'
 
 const handleEmail = async(email) => {
 
